39a854a1d9efcbe2bb6d29d3db00bd794556cd75
Sanaullah Ansari <asanaullah129@gmail.com>
2024-05-23 22:43:07 +0530

    Initialized Project. Database connectivity & backend setup completed

---
+++ backend/server/server.ts
@@ -0,0 +1,24 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express, { Express } from 'express';
+import pool from './database';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT ?? 3200;
+
+app.use(express.json());
+app.use(cors);
+
+pool.query("SELECT * FROM vehicles")
+    .then(([rows, fields]) => {
+        console.log(rows);
+    })
+    .catch(error => {
+        console.log("SQL Error: " + error);
+    });
+
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`)
+})
